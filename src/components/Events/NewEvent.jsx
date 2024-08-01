2063b422a802a52aba95c78776823b48c7ca9b20
amr4356 <amrooo73321@gmail.com>
2024-08-01 03:50:32 +0300

    398. Acting on Mutation Success & Invalidating Queries

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -3,18 +3,23 @@ import { useMutation } from '@tanstack/react-query';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
-import { createNewEvent } from '../../util/http.js';
+import { createNewEvent, queryClinet } from '../../util/http.js';
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
   const {mutate, isPending, isError, error}=useMutation({
-    mutationFn: createNewEvent
+    mutationFn: createNewEvent,
+    onSuccess: () => {
+      queryClinet.invalidateQueries({queryKey: ['events']});
+      navigate('/events');
+    }
   });
 
   function handleSubmit(formData) {
     mutate({event: formData})
+    
   }
 
   return (
