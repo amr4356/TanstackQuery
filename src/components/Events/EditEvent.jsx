3b9c909fdbf387179d3a1ffac8e34c48907c4b13
amr4356 <amrooo73321@gmail.com>
2024-08-02 04:40:42 +0300

    405. Optimistic Updating

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -2,7 +2,7 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
-import { fetchEvent, updateEvent } from '../../util/http.js';
+import { fetchEvent, queryClient, updateEvent } from '../../util/http.js';
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 import { useMutation, useQuery } from '@tanstack/react-query';
@@ -18,6 +18,21 @@ export default function EditEvent() {
 
   const {mutate}=useMutation({
     mutationFn:updateEvent,
+    onMutate: async (data) => {
+      const newEvent=data.event;
+      await queryClient.cancelQueries({queryKey: ['events',params.id]});
+      const previousEvent=queryClient.getQueryData(['events',params.id]);
+
+      queryClient.setQueryData(['events',params.id], newEvent  );
+
+      return {previousEvent}
+    },
+    onError: (error, data, context) =>{
+      queryClient.setQueryData(['events',params.id], context.previousEvent);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(['events',params.id]);
+    }
   });
 
   function handleSubmit(formData) {
